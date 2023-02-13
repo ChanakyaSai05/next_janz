e00971ea8cb0439ebda7512e12d9c19993ae1c5b
chanakyasai <chanakya@infiniticube.in>
2023-02-13 11:18:03 +0530

    changed next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: "/shop_by_brand/:brandId",
-        destination: "/shop_by_brand/:brandId", // Matched parameters can be used in the destination
+        destination: "/shop_by_brand/:brandId",
       },
     ];
   },
